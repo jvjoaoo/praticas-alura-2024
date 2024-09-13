e94daee0ce52a4243d19f50b832cc89b4dc3c8a7
João Vitor <62124487+jvjoaoo@users.noreply.github.com>
2024-09-13 14:45:35 -0300

    Create pratica01.js

---
+++ do-zero-iniciando-em-programacao/logica-js-projeto_inicial/desafios-js/pratica01.js
@@ -0,0 +1,26 @@
+//1- Desenvolva uma aplicação que receba o nome do usuário e retorne uma mensagem de boas-vindas com o nome dele:
+
+//2- Crie um sistema que recebe o valor da compra que o usuário gostaria de fazer:
+
+//   2.1- Faça a formatação para moeda em 'BR';
+//   2.2- Crie duas variaveis armazenando dois setores diferentes; 
+//   2.3- Crie a condicional:
+//    Se o valor da compra for maior que determinado valor
+//      usuário será direcionado para setor 1 -> exibir com console.log
+//    Se não
+//      usuário será direcionado para setor 2 -> exibir com console.log
+
+const userName = prompt("Login: ")
+alert(`Seja bem-vindo ${userName}, vamos praticar JavaScript!`)
+
+let valorCompra = parseInt(prompt("Qual o valor da compra: "))
+let valorFormatado = valorCompra.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+
+const setorUm = "Compras"
+const setorDois = "Licitações"
+
+if(valorCompra >= 64.000){
+console.log(`Desculpe ${userName}, não podemos prosseguir com o seu atendimento o valor da sua compra excede o limite. Você foi direcionado ao setor de ${setorDois} para obter a ajuda necessária`)
+} else {
+console.log(`Olá ${userName} o senhor fez uma compra de ${valorFormatado}, gostaria de dar continuidade no atendimento?`)
+}
