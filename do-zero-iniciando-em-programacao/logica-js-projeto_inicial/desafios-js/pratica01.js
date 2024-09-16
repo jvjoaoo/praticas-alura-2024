1f64493e0a40e08b277ebfc8ffec0a191d45be8a
jvjoaoo <jvjoaoo.vitor@gmail.com>
2024-09-15 21:53:28 -0300

    Adicionado anotações sobre laços de repetição

--- do-zero-iniciando-em-programacao/logica-js-projeto_inicial/desafios-js/pratica01.js
+++ do-zero-iniciando-em-programacao/logica-js-projeto_inicial/desafios-js/pratica01.js
@@ -3,7 +3,7 @@
 //2- Crie um sistema que recebe o valor da compra que o usuário gostaria de fazer:
 
 //   2.1- Faça a formatação para moeda em 'BR';
-//   2.2- Crie duas variaveis armazenando dois setores diferentes; 
+//   2.2- Crie duas variaveis armazenando dois setores diferentes;
 //   2.3- Crie a condicional:
 //    Se o valor da compra for maior que determinado valor
 //      usuário será direcionado para setor 1 -> exibir com console.log
@@ -14,15 +14,22 @@ const userName = prompt("Login: ")
 alert(`Seja bem-vindo ${userName}, vamos praticar JavaScript!`)
 
 let valorCompra = parseInt(prompt("Qual o valor da compra: "))
-let valorFormatado = valorCompra.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+let valorFormatado = valorCompra.toLocaleString("pt-br", {
+  style: "currency",
+  currency: "BRL",
+})
 
 const setorUm = "Compras"
 const setorDois = "Licitações"
 
-if(valorCompra >= 64.000){
-console.log(`Desculpe ${userName}, não podemos prosseguir com o seu atendimento o valor da sua compra excede o limite. Você foi direcionado ao setor de ${setorDois} para obter a ajuda necessária`)
+if (valorCompra >= 64.0) {
+  alert(
+    `Desculpe ${userName}, não podemos prosseguir com o seu atendimento o valor da sua compra excede o limite. Você foi direcionado ao setor de ${setorDois} para obter a ajuda necessária`
+  )
 } else {
-console.log(`Olá ${userName} o senhor fez uma compra de ${valorFormatado}, gostaria de dar continuidade no atendimento?`)
+  alert(
+    `Olá ${userName} o senhor fez uma compra de ${valorFormatado}, gostaria de dar continuidade no atendimento?`
+  )
 }
 
 //Bugs com o código acima:
