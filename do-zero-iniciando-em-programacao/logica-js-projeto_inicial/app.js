f56a90b4235fd2d61badf1d5c4ce90cd77e108b2
João Vitor <62124487+jvjoaoo@users.noreply.github.com>
2024-09-25 12:26:37 -0300

    Update app.js - final de projeto Jogo do número Secreto

--- do-zero-iniciando-em-programacao/logica-js-projeto_inicial/app.js
+++ do-zero-iniciando-em-programacao/logica-js-projeto_inicial/app.js
@@ -1,11 +1,14 @@
+let listaDeNumerosSorteados = []
+let numeroLimite = 5
 let numeroSecreto = gerarNumeroAleatorio()
 let tentativas = 1;
 console.log(numeroSecreto)
 
 // funções com parâmetros em JavaScript
 function exibirTextoNaTela(tag, texto) {
-  let campo = document.querySelector(tag)
+  let campo = document.querySelector(tag, texto)
   campo.innerHTML = texto
+  responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2})
 }
 function exibirMensagemInicial(){
   exibirTextoNaTela("h1", "Jogo do número secreto")
@@ -37,7 +40,20 @@ function verificarChute() {
 
 //Funções com retorno
 function gerarNumeroAleatorio() {
-  return parseInt(Math.random() * 5 + 1)
+  let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1)
+  let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length
+
+  if(quantidadeDeElementosNaLista == numeroLimite){
+    listaDeNumerosSorteados = []
+  }
+
+  if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+    return gerarNumeroAleatorio()
+  } else {
+    listaDeNumerosSorteados.push(numeroEscolhido)
+    console.log(listaDeNumerosSorteados) 
+    return numeroEscolhido
+  }
 }
 
 function limparCampo(){
